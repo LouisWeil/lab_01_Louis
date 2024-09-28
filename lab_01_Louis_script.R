ce9f9d850fed12da4541a80c7e1607252568b541
Louis Newton <s2474470@ed.ac.uk>
2024-09-28 13:55:53 +0100

    Finished non-challenging exercises

--- lab_01_Louis_script.R
+++ lab_01_Louis_script.R
@@ -9,3 +9,10 @@ View(visitors)
 arrange(visitors, desc(n_2022))
 
 select(visitors, startsWith("National"))
+
+visitors |> 
+  filter(n_2022 > 1000000) |> 
+  count(region)
+
+visitors |> 
+  count(region)
